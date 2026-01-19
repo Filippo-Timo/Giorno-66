efeb76b14312ce86aa97f13b6b128fb3457557ed
Filippo-Timo <filtimo2002@gmail.com>
2026-01-19 15:28:16 +0100

    Creates repo

---
+++ src/main/java/filippotimo/Application.java
@@ -0,0 +1,19 @@
+package filippotimo;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        /*
+         * Esercizio 1)
+         * SELECT * FROM clienti WHERE nome = "Mario"
+         *
+         * Esercizio 2)
+         *  SELECT nome, cognome WHERE anno_di_nascita = 1982
+         *
+         * Esercizio 3)
+         *
+         *  */
+
+    }
+}

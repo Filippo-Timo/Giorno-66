cad38312f5c8dcfb43118a16ba0a3d2c69cff67f
Filippo-Timo <filtimo2002@gmail.com>
2026-01-19 16:12:41 +0100

    Finishes four exercises

--- src/main/java/filippotimo/Application.java
+++ src/main/java/filippotimo/Application.java
@@ -6,12 +6,37 @@ public class Application {
 
         /*
          * Esercizio 1)
-         * SELECT * FROM clienti WHERE nome = "Mario"
+         *
+         * SELECT * FROM clienti WHERE nome = 'Mario'
+         *
          *
          * Esercizio 2)
-         *  SELECT nome, cognome WHERE anno_di_nascita = 1982
+         *
+         * SELECT nome, cognome FROM clienti WHERE anno_di_nascita = 1982
+         *
          *
          * Esercizio 3)
+         *
+         * SELECT COUNT (*) FROM fatture WHERE iva = 20
+         *
+         *
+         * Esercizio 4)
+         *
+         * SELECT (*) FROM prodotti WHERE extract(year from data_attivazione) = 2017 AND (in_produzione = true OR in_commercio = true)
+         *
+         *
+         * Esercizio 5)
+         *
+         * SELECT * FROM fatture
+         * JOIN clienti ON fatture.id_cliente = clienti.numero_cliente
+         * WHERE importo < 1000
+         *
+         *
+         *  Esercizio 6)
+         *
+         *
+         * SELECT numero_fattura,importo,iva,data_fattura,denominazione FROM fatture
+         * JOIN fornitori USING (numero_fornitore)
          *
          *  */
 
